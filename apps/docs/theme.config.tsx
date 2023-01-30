47b205a634598dae97ce18978a768803231080dd
Alireza Sarabchi <a.sarabchi@gmail.com>
2023-01-30 19:45:46 +0330

    fix?

--- apps/docs/theme.config.tsx
+++ apps/docs/theme.config.tsx
@@ -1,17 +1,21 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
 import Preview from 'components/Preview'
 import Overview from 'components/Overview'
-import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>@noqte/ui Documentations</span>,
-  docsRepositoryBase: 'https://github.com/noqte/ui/apps/docs',
   darkMode: false,
+  docsRepositoryBase: 'https://github.com/noqte/ui/apps/docs',
   components: {
     Preview: Preview,
     Overview: Overview
   },
   project: {
     link: 'https://github.com/noqte/ui'
+  },
+  footer: {
+    text: '@noqte/ui Documentations'
   }
 }
 
